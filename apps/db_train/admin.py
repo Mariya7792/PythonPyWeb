fa2ed6ba5b6637f63235fb528f49150998971bee
Mariya7792 <sophi5@yandex.ru>
2024-04-04 21:23:05 +0300

    Практика 2, задание 1

--- apps/db_train/admin.py
+++ apps/db_train/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 from .models import Author
 admin.site.register(Author)
 
+from .models import AuthorProfile
+admin.site.register(AuthorProfile)
 
+from .models import Entry
+admin.site.register(Entry)
+
+from .models import Tag
+admin.site.register(Tag)
 # Зарегистрируйте свои модели в админ панели здесь
